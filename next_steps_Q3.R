1c0a807e7396e492bdc42d8f2571b636acf3d682
Tom Prendergast (tier0) <tier0\tom.prendergast@ec2amaz-9u4wpc.tier0.prd.thf.aws>
2024-07-31 14:13:24 +0000

    Added next steps Q3 script

---
+++ next_steps_Q3.R
@@ -0,0 +1,17 @@
+### QUESTION 3: HOW DO DIAGNOSTIC WAITING TIMES COMPARE BETWEEN AREAS WITH AND WITHOUT CDCs?
+
+
+# Step 1: In joined DM01/CDC data, create an additional variable which flags which trusts have a CDC associated with them, and which do not
+
+
+# Step 2: A simple comparison of the distribution of waiting times for each test between trusts which have a CDC compared to those which don't, first presented in the aggregate.  
+# There are a few different ways of approaching this - waiting times can be treated as a categorical variable, as they are presented in the data, or they can be 
+# converted into numbers. If converting to numbers, box and whisker graphs would be a good way to compare. 
+
+
+# Step 3: Create a scatter plot (trust and test level) of average waiting times vs proportion of activity performed by CDCs.
+# Waiting times will need to be numeric to compute averages. If a lot of trusts have no associated CDC, it may be an idea to only look at trusts with CDCs. 
+
+
+
+# Step 4: Difference-in-difference comparisons? We'll discuss this when I'm back from leave
