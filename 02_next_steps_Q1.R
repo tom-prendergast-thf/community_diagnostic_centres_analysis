626043bcc07f0441faa60cd408c30179bcdafdc2
tom-prendergast-thf <141746852+tom-prendergast-thf@users.noreply.github.com>
2024-07-31 12:43:17 +0100

    Update 02_next_steps_Q1.R

--- 02_next_steps_Q1.R
+++ 02_next_steps_Q1.R
@@ -7,7 +7,7 @@ cdc_trust_lookup <- read.csv('resources/CDC trust lookup.csv')
 
 
 # Step 2: Join DM01 data to CDC data
-# This join will need to be performed based on the trust, date and test type
+# This join will need to be performed based on the trust, date and test type. The trust codes tend to be more reliable for joining in case of any typos/differences in capitalisation between the trust names in different sources.  
 # Best approach would probably be a left join, with the DM01 data as the left dataset
 
 
@@ -21,15 +21,15 @@ cdc_trust_lookup <- read.csv('resources/CDC trust lookup.csv')
 
 
 # Step 4: Visualise portion of CDC activity by test type
-# It would be interesting to see this overall (or for select time period, say the past 12 months), with a horizontal bar chart
+# It would be interesting to see this overall (or for select time period, say total of the past 12 months), with a horizontal bar chart
 # And time series for test types would be good as well
 
 
 
 
-# Step 5: Visualise portion of CDC activity by ICB
-# Trust level is also of interest, but won't be easily visualised as there's so many of them (aside from showing a distribution, or in comparison to another variable on a scatter plot)
-
+# Step 5: Visualise portion of CDC activity by ICB and trust.
+# ICB level would be interesting on a bar chart (either totals or monthly averages rather than over time)
+# Trust level could be visualised either through a distribution chart/histogram, or a scatter plot in comparison to any other variables of interest (there may not be any for now)
 
 
 
